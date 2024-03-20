ecc3a815c10571f31f3dd4ee0339f609d5cd0cb1
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2024-03-20 00:47:52 -0500

    refactor: slim down package until find a `chat.db`-less solution for message tracking

--- i_py_messenger/imessage_client.py
+++ i_py_messenger/imessage_client.py
@@ -1,13 +1,10 @@
 import subprocess
 from .configuration import Configuration
-from .db_manager import DBManager
-from .message import Message
 
 
 class IMessageClient:
     def __init__(self, config: Configuration):
         self.config = config
-        self.db_manager = DBManager(config)
 
     def send(self, phone_number: str, message: str) -> bool:
         cmd = f'osascript "{self.config.send_script_path}" "{phone_number}" "{message}"'
@@ -24,15 +21,3 @@ class IMessageClient:
             return result == "true"
         except subprocess.CalledProcessError:
             return False
-
-    def get_most_recently_sent_text(self) -> str:
-        return self.db_manager.get_most_recently_sent_text()
-
-    def get_message(self, guid: str) -> Message | None:
-        return self.db_manager.get_message(guid)
-
-    def get_messages_for_phone_number(self, phone_number: str) -> list[Message]:
-        return self.db_manager.get_messages_for_phone_number(phone_number)
-
-    def close(self):
-        self.db_manager.close_connection()
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from i_py_messenger import IMessageClient, Configuration
 from dotenv import dotenv_values
 
 
-def test_send_message(client, phone_number, message_text):
+def test_send_message(client: IMessageClient, phone_number, message_text):
     success = client.send(phone_number, message_text)
     if success:
         print("Message sent successfully.")
@@ -10,7 +10,7 @@ def test_send_message(client, phone_number, message_text):
         print("Failed to send message.")
 
 
-def test_check_compatibility(client, phone_number):
+def test_check_compatibility(client: IMessageClient, phone_number):
     is_compatible = client.check_compatibility(phone_number)
     if is_compatible:
         print(f"{phone_number} is compatible with iMessage.")
@@ -18,47 +18,17 @@ def test_check_compatibility(client, phone_number):
         print(f"{phone_number} is not compatible with iMessage.")
 
 
-def test_get_most_recently_sent_text(client):
-    most_recent_text = client.get_most_recently_sent_text()
-    if most_recent_text:
-        print(f"Most recently sent text: {most_recent_text}")
-    else:
-        print("No recently sent text found.")
-
-
-def test_get_messages_for_phone_number(client):
-    phone_number = input("Enter a phone number to retrieve messages: ")
-    messages = client.get_messages_for_phone_number(phone_number)
-    if messages:
-        print(f"Messages for {phone_number}:")
-        for message in messages:
-            print(f"- {message.date}: {message.guid}")
-    else:
-        print(f"No messages found for {phone_number}.")
-
-
 def run_tests():
-    os_password = dotenv_values().get("OS_PASSWORD")
     test_phone_number = dotenv_values().get("TEST_PHONE_NUMBER")
 
-    config = Configuration(
-        os_password=os_password,
-    )
-    client = IMessageClient(config)
+    config = Configuration()
+    client: IMessageClient = IMessageClient(config)
 
     print("Running send message test...")
     test_send_message(client, test_phone_number, "Test message from i_py_messenger.")
 
     print("\nRunning compatibility test...")
     test_check_compatibility(client, test_phone_number)
-
-    print("\nRunning get most recently sent text test...")
-    test_get_most_recently_sent_text(client)
-
-    print("\nRunning get messages for phone number test...")
-    test_get_messages_for_phone_number(client)
-
-    client.close()
 
 
 if __name__ == "__main__":

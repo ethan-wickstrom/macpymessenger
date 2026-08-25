0065960819b6469496a8d42ba14a52a8caa82073
Ethan Wickstrom <143438547+ethan-wickstrom@users.noreply.github.com>
2026-08-25 23:45:02 +0000

    test: remove obsolete logging cleanup helper

--- tests/support.py
+++ tests/support.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import logging
 import subprocess
 from typing import TYPE_CHECKING
 
@@ -11,21 +10,11 @@ if TYPE_CHECKING:
 class StubRunner:
     def __init__(self, failing_recipient_handles: Sequence[str] | None = None) -> None:
         self.commands: list[list[str]] = []
-        if failing_recipient_handles is None:
-            self.failing_recipient_handles = set()
-        else:
-            self.failing_recipient_handles = set(failing_recipient_handles)
+        self.failing_recipient_handles = set(failing_recipient_handles or ())
 
     def __call__(self, command: Sequence[str]) -> None:
         arguments = list(command)
         self.commands.append(arguments)
-        recipient_handle = arguments[2]
-        if recipient_handle in self.failing_recipient_handles:
+        recipient = arguments[2]
+        if recipient in self.failing_recipient_handles:
             raise subprocess.CalledProcessError(returncode=1, cmd=arguments)
-
-
-def remove_file_handlers(logger: logging.Logger) -> None:
-    for handler in list(logger.handlers):
-        if isinstance(handler, logging.FileHandler):
-            handler.close()
-            logger.removeHandler(handler)
